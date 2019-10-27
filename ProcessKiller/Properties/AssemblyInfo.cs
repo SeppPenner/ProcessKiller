442ac50cbcb71e5ad68ba90797a0a3b961f75edc
SeppPenner <HansM2013@gmx.de>
2019-10-27 20:49:10 +0100

    Updated nuget packages, added GitVersionTask.

--- ProcessKiller/Properties/AssemblyInfo.cs
+++ ProcessKiller/Properties/AssemblyInfo.cs
@@ -23,17 +23,3 @@ using System.Runtime.InteropServices;
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 
 [assembly: Guid("3586ca06-782d-4db3-84d4-fc6e7cab36e5")]
-
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion 
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
